209cc3832c9a5751e13ce777128a875fd6469531
Brent Brewington <brent.brewington@gmail.com>
2016-06-14 22:51:10 -0400

    Update bing-traffic-api.R

--- bing-traffic-api.R
+++ bing-traffic-api.R
@@ -1,4 +1,5 @@
 library(httr)
+library(dplyr)
 
 key <- "YOUR_BING_KEY" #To set one up, go to www.bingmapsportal.com
 
@@ -8,8 +9,16 @@ r <- GET(paste0("http://dev.virtualearth.net/REST/v1/Traffic/Incidents/",
                 "33.629548,-84.512112,33.927471,-84.242947/true?",
                 "t=1,2,3,4,5,6,7,8,9,10,11&s=1,2,3,4&key=", key, "&o=json"))
 
-request.time <- headers(r)$date
+headers(r)
 
 response <- content(r)
 
-# TO DO: Flatten response data into a data frame
+# Create data frame of some key columns from the response
+# TO DO: Figure out how to collapse location data (some are path-based, some are point based it seems)
+response.data <- lapply(response$resourceSets[[1]]$resources, 
+                        function(x) data.frame(x$description, x$start, x$end, x$lastModified, 
+                                               x$type, x$severity, x$incidentId)) %>% rbind_all()
+
+response.data
+
+write.csv(response.data, "bing.data.csv")
